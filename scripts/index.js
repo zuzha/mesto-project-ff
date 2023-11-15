2c28834264669c76ff6382f69454a2d466a30ff2
Alena Zhiganova <me@zhiganova.ru>
2023-11-15 15:37:54 +0400

    Fixed function names.

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,15 @@ const placesList = document.querySelector('.places__list');
 
 
 // @todo: Функция создания карточки
-function addCard (element, remove) {
+function createCard (element, remove) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     
   cardElement.querySelector('.card__title').textContent = element.name;
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelector('.card__image').alt = element.name;
+
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
   
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', remove);
@@ -30,10 +33,10 @@ function removeCard (event) {
 
 
 // @todo: Вывести карточки на страницу
-function renderList() {
+function renderInitialCards() {
   initialCards.forEach((element) => {
-    placesList.append(addCard(element, removeCard));
+    placesList.append(createCard(element, removeCard));
   });
 }
 
-renderList();
+renderInitialCards();
