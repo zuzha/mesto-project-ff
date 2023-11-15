1522964a77e1513b13e41c1eca210fda2721f1e2
Alena Zhiganova <me@zhiganova.ru>
2023-11-15 08:31:48 +0400

    Added functionality for creating and deleting cards.

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Сеул, Южная Корея",
+    link: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663395324_11-mykaleidoscope-ru-p-stolitsa-yuzhnoi-korei-seul-oboi-12.jpg",
+  },
+  {
+    name: "Нью Йорк, США",
+    link: "https://sportishka.com/uploads/posts/2022-04/1650711334_2-sportishka-com-p-nyu-york-siti-krasivo-foto-2.jpg",
+  },
+  {
+    name: "Осло, Норвегия",
+    link: "https://traveltimes.ru/wp-content/uploads/2021/07/nor-1536x1025.jpg",
+  },
+  {
+    name: "Токио, Япония",
+    link: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663357946_19-mykaleidoscope-ru-p-tokio-stolitsa-yaponii-instagram-20.jpg",
+  },
+  {
+    name: "Париж, Франция",
+    link: "https://sportishka.com/uploads/posts/2022-04/1650488882_23-sportishka-com-p-frantsiya-parizh-dostoprimechatelnosti-kra-26.jpg",
+  },
+  {
+    name: "ЧеджуДо, Южная Корея",
+    link: "https://sportishka.com/uploads/posts/2022-11/1667573886_11-sportishka-com-p-ostrov-chedzhudo-yuzhnaya-koreya-vkontakte-12.jpg",
+  }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
+function addCard (element, remove) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', remove);
+
+  return cardElement;
+}
+
 
 // @todo: Функция удаления карточки
+function removeCard (event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
+
 
 // @todo: Вывести карточки на страницу
+function renderList() {
+  initialCards.forEach((element) => {
+    placesList.append(addCard(element, removeCard));
+  });
+}
+
+renderList();
